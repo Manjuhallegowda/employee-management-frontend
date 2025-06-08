7b6b3440e5625fe8f221e2d6ee0f4517d8020674
Manjuhallegowda <142666175+Manjuhallegowda@users.noreply.github.com>
2025-06-08 14:03:22 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://employee-management-backend-89cq.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
